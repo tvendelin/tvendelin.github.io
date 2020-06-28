5030db81e1de861d3d5492ae17aa4f80a81feb80
Toomas Vendelin <toomas@vendelin.com>
2020-06-28 19:45:55 +0200

    GA and GDPR

--- js/hamburger.js
+++ js/hamburger.js
@@ -39,3 +39,67 @@ function switchTab(ev, target){
     document.getElementById(target).style.display = "block";
     ev.currentTarget.className += ACTIVE
 }
+
+function closeGdprBanner() {
+    document.getElementById('gdpr').style.display = 'none'
+}
+
+function gdprOK(){
+    // GA
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-162948019-2');
+
+    console.log('gdprOK');
+}
+
+function gdprNOK(){
+    console.log('gdprNOK');
+    // GA
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-162948019-2', {
+        'client_storage' : 'none'
+    });
+
+}
+
+function cookieDecision() {
+    return document.cookie
+        .split('; ')
+        .find(row => row.startsWith('accepting'));
+}
+
+window.addEventListener('load', function(event){
+    var decn = cookieDecision();
+
+    if(decn) {
+        closeGdprBanner();
+        const accept = decn.split('=')[1];
+        accept == 1 ? gdprOK() : gdprNOK();
+    }
+
+    document.getElementById('reject').addEventListener('click', function (){
+        document.cookie = 'accepting=0;path=/;samesite=strict;';
+        gdprNOK();
+        closeGdprBanner();
+    });
+
+    document.getElementById('accept').addEventListener('click', function (){
+        document.cookie = 'accepting=1;path=/;max-age=31536000;samesite=strict;';
+        gdprOK();
+        closeGdprBanner();
+    });
+});
+
+window.addEventListener('unload', function(event){
+    var decn = cookieDecision();
+    if(decn == null){
+        gdprNOK()
+    }
+})
+
